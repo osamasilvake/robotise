910e760f6c52b969c16dceecc925032acd2222dc
Imran Khan <imran.khan@robotise.eu>
2021-04-03 12:53:35 +0200

    fix: Store redux init state

--- packages/client/src/app/slices/index.tsx
+++ packages/client/src/app/slices/index.tsx
@@ -1,11 +1,13 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { AnyAction, combineReducers } from 'redux';
+import ReduxLogger from 'redux-logger';
 
-import auth, { initialState as aState } from './auth/Auth.slice';
+import auth from './auth/Auth.slice';
 import general from './general/General.slice';
-import robot, { initialState as rState } from './robot/Robot.slice';
-import robotTwins, { initialState as rtState } from './robot-twins/RobotTwins.slice';
-import robotTwinsSummary, { initialState as rtbState } from './robot-twins/RobotTwinsSummary.slice';
-import sites, { initialState as sState } from './sites/Sites.slice';
+import robot from './robot/Robot.slice';
+import robotTwins from './robot-twins/RobotTwins.slice';
+import robotTwinsSummary from './robot-twins/RobotTwinsSummary.slice';
+import sites from './sites/Sites.slice';
 
 // app reducers
 const combinedReducer = combineReducers({
@@ -21,21 +23,25 @@ const combinedReducer = combineReducers({
 export type AppReducerType = ReturnType<typeof combinedReducer>;
 
 // root reducer
+let initRootState: AppReducerType | undefined = undefined;
 const rootReducer = (rootState: AppReducerType | undefined, action: AnyAction) => {
 	// terminate the state of a redux store
 	if (action.type === 'Auth/terminate') {
-		if (rootState) {
+		if (rootState && initRootState) {
 			rootState = {
-				...rootState,
-				auth: aState,
-				general: rootState.general,
-				robot: rState,
-				robotTwins: rtState,
-				robotTwinsSummary: rtbState,
-				sites: sState
+				...initRootState,
+				general: rootState.general
 			};
 		}
 	}
 	return combinedReducer(rootState, action);
 };
-export default rootReducer;
+
+// configure store
+export const store = configureStore({
+	reducer: rootReducer,
+	middleware: [...getDefaultMiddleware(), ReduxLogger]
+});
+
+// store redux initial state
+initRootState = store.getState();
--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,18 +1,10 @@
 import './i18n/i18n';
 
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import ReduxLogger from 'redux-logger';
 
 import App from './app/App';
-import rootReducer from './app/slices';
-
-// configure store
-export const store = configureStore({
-	reducer: rootReducer,
-	middleware: [...getDefaultMiddleware(), ReduxLogger]
-});
+import { store } from './app/slices';
 
 ReactDOM.render(
 	<Provider store={store}>

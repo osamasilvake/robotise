adac58b60c6a01784dc7d50c652d44ed4d1ec845
Imran Khan <imran.khan@robotise.eu>
2022-01-12 15:18:07 +0100

    refactor: Rename site operations interface

--- src/app/screens/business/sites/content/configuration/accept-orders/AcceptOrders.interface.tsx
+++ src/app/screens/business/sites/content/configuration/accept-orders/AcceptOrders.interface.tsx
@@ -1,7 +1,7 @@
-import { SliceOperationsSiteInterface } from '../../../../../../slices/business/sites/SiteOperations.slice.interface';
+import { SliceSiteOperationsInterface } from '../../../../../../slices/business/sites/SiteOperations.slice.interface';
 import { SliceSitesInterface } from '../../../../../../slices/business/sites/Sites.slice.interface';
 
 export interface AcceptOrdersInterface {
 	sites: SliceSitesInterface;
-	siteOperations: SliceOperationsSiteInterface;
+	siteOperations: SliceSiteOperationsInterface;
 }

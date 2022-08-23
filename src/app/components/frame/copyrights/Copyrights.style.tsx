8623e29c093e4c6019f5383bc643af60a673548a
Imran Khan <imrankhan@Imrans-MacBook-Pro.local>
2022-08-23 17:19:01 +0200

    fix: Copyright style

--- src/app/components/frame/copyrights/Copyrights.style.tsx
+++ src/app/components/frame/copyrights/Copyrights.style.tsx
@@ -1,6 +1,7 @@
+import { Theme } from '@mui/material/styles';
 import { makeStyles } from '@mui/styles';
 
-export const CopyrightStyle = makeStyles(() => ({
+export const CopyrightStyle = makeStyles((theme: Theme) => ({
 	sCopyright: {
 		height: 40
 	},

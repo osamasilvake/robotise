7703ec100cfc7211daea5f22f996b80e689b1945
Deni Begaj <deni.begaj@robotise.eu>
2022-08-23 19:10:32 +0200

    chore: Add last2M and last3M filters in wifi stats

--- src/app/screens/business/sites/content/statistics/wifi-heatmap/SiteStatisticsWifiHeatmap.enum.tsx
+++ src/app/screens/business/sites/content/statistics/wifi-heatmap/SiteStatisticsWifiHeatmap.enum.tsx
@@ -1,5 +1,7 @@
 export enum SiteStatisticsWifiHeatmapPeriodsTypeEnum {
 	LAST_DAY = 'now-1d',
 	LAST_WEEK = 'now-7d',
-	LAST_MONTH = 'now-30d'
+	LAST_MONTH = 'now-30d',
+	LAST_2_MONTHS = 'now-2M',
+	LAST_3_MONTHS = 'now-3M'
 }

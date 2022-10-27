36f653830fb8e893f4941289daa6034b25eb20c3
Imran Khan <imransilvake@gmail.com>
2022-10-27 14:18:49 +0200

    fix: Update packages

--- src/app/components/common/status/Status.interface.tsx
+++ src/app/components/common/status/Status.interface.tsx
@@ -1,7 +1,9 @@
+import { DefaultTFuncReturn } from 'i18next';
+
 import { StatusTypeEnum } from './Status.enum';
 
 export interface StatusInterface {
-	children: string | number;
+	children: DefaultTFuncReturn | string | number;
 	small?: boolean;
 	active?: boolean;
 	capitalize?: boolean;
--- src/app/components/frame/error-boundary/ErrorBoundary.interface.tsx
+++ src/app/components/frame/error-boundary/ErrorBoundary.interface.tsx
@@ -1,5 +1,5 @@
+import { TFunction } from 'i18next';
 import { ReactNode } from 'react';
-import { TFunction } from 'react-i18next';
 
 import { ErrorTypeEnum } from '../../common/error/Error.enum';
 

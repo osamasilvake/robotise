2325ba0c44915dfa0c23a860048e0126e186c2b4
Imran Khan <imransilvake@gmail.com>
2022-11-14 15:56:18 +0100

    fix: Meta title

--- src/app/components/content/page-head/PageHead.tsx
+++ src/app/components/content/page-head/PageHead.tsx
@@ -16,7 +16,7 @@ const PageHead: FC<PageHeadInterface> = (props) => {
 		<Box>
 			{/* Meta */}
 			<Meta
-				title={t(title)}
+				title={title ? t(title) : ''}
 				description={(description && t(description)) || t('COMMON.DESCRIPTION')}
 			/>
 

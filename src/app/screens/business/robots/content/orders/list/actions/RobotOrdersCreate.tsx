a1c15ae08d409daf4ab6b3c553b3ed4280529e0b
Imran Khan <imransilvake@gmail.com>
2022-11-16 16:41:42 +0100

    fix: Robot not assigned to site, show error

--- src/app/screens/business/robots/content/orders/list/actions/RobotOrdersCreate.tsx
+++ src/app/screens/business/robots/content/orders/list/actions/RobotOrdersCreate.tsx
@@ -2,15 +2,49 @@ import { Add } from '@mui/icons-material';
 import { Chip } from '@mui/material';
 import { FC, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
+import { TriggerMessageTypeEnum } from '../../../../../../../components/frame/message/Message.enum';
+import { TriggerMessageInterface } from '../../../../../../../components/frame/message/Message.interface';
+import { AppDispatch } from '../../../../../../../slices';
+import { AppTriggerMessage } from '../../../../../../../slices/app/App.slice';
+import { robotTwinsSummarySelector } from '../../../../../../../slices/business/robots/RobotTwinsSummary.slice';
+import { RobotParamsInterface } from '../../../../Robot.interface';
 import DialogCreateOrder from './DialogCreateOrder';
 
 const RobotOrdersCreate: FC = () => {
 	const { t } = useTranslation('GENERAL');
 
-	const [createOrder, setCreateOrder] = useState(false);
+	const dispatch = useDispatch<AppDispatch>();
+	const robotTwinsSummary = useSelector(robotTwinsSummarySelector);
 
+	const [createOrder, setCreateOrder] = useState(false);
+	const params = useParams<keyof RobotParamsInterface>() as RobotParamsInterface;
+
+	const cRobotId = params.robotId;
+	const isRobotAssignedToSite = robotTwinsSummary.content?.dataById[cRobotId]?.siteId;
 	const translation = 'COMMON.ORDERS.LIST.ACTIONS.CREATE';
+
+	/**
+	 * handle create order
+	 */
+	const handleCreateOrder = () => {
+		if (isRobotAssignedToSite) {
+			// set create order
+			setCreateOrder(true);
+		} else {
+			// dispatch: trigger message
+			const message: TriggerMessageInterface = {
+				id: 'robot-assign-to-site',
+				show: true,
+				severity: TriggerMessageTypeEnum.ERROR,
+				text: 'GENERAL.COMMON.ROBOT_NOT_ASSIGNED_TO_SITE'
+			};
+			dispatch(AppTriggerMessage(message));
+		}
+	};
 
 	return (
 		<>
@@ -21,7 +55,7 @@ const RobotOrdersCreate: FC = () => {
 				color="primary"
 				variant="outlined"
 				clickable
-				onClick={() => setCreateOrder(true)}
+				onClick={handleCreateOrder}
 			/>
 
 			{/* Dialog: Create Order */}

49746b825fb8b409cb501d67774aaaac604440b4
Imran Khan <imransilvake@gmail.com>
2022-11-17 15:50:51 +0100

    fix: Sort the list of locations

--- src/app/screens/business/sites/content/configuration/marketing-rides/SiteConfigurationMarketingRidesAutocomplete.tsx
+++ src/app/screens/business/sites/content/configuration/marketing-rides/SiteConfigurationMarketingRidesAutocomplete.tsx
@@ -56,7 +56,7 @@ const SiteConfigurationMarketingRidesAutocomplete: FC<
 				id="locations"
 				options={options}
 				isOptionEqualToValue={(option, value) => option === value}
-				value={locations || []}
+				value={locations?.sort((a, b) => a?.localeCompare(b)) || []}
 				onChange={(_, values) => handleChangeInputs('locations', values)}
 				onBlur={handleBlur}
 				renderInput={(params) => (

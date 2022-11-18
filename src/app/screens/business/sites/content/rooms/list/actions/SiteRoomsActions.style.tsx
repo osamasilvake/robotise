94b737ae1df17e0645e559e2d62871cb2bb6f506
Imran Khan <imransilvake@gmail.com>
2022-11-18 10:23:08 +0100

    revert: For Rooms sticky header

--- src/app/screens/business/sites/content/rooms/list/actions/SiteRoomsActions.style.tsx
+++ src/app/screens/business/sites/content/rooms/list/actions/SiteRoomsActions.style.tsx
@@ -1,9 +1,14 @@
 import { Theme } from '@mui/material/styles';
 import { makeStyles } from '@mui/styles';
 
+import { AppConfigService } from '../../../../../../../services';
+
 export const SiteRoomsActionsStyle = makeStyles((theme: Theme) => ({
 	sActions: {
 		marginBottom: theme.spacing(2),
-		padding: theme.spacing(0.5, 2)
+		padding: theme.spacing(0.5, 2),
+		position: 'sticky',
+		top: 0,
+		zIndex: AppConfigService.AppOptions.styles.zIndex.level3
 	}
 }));

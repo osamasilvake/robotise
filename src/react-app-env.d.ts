200e8789884e97d614967577c894ae523a30e945
Imran Khan <imransilvake@gmail.com>
2022-11-29 09:31:54 +0100

    chore: Update packages

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,6 +1,3 @@
 // a directive declares a dependency on a package
 // triple-slash-reference-types directives is as an import for declaration packages
 /// <reference types="react-scripts" />
-
-// HTML to Image
-declare module 'dom-to-image';
